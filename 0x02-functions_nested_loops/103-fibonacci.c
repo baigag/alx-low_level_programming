4fcb212239e8477febf3dae1b7387c0039731670
baigag <106968031+baigag@users.noreply.github.com>
2022-09-14 13:29:38 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,26 +1,13 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include "holberton.h"
 /**
-*main - prints out first 50
-*fibonacci suit numbers
-*Return: return 0
-*/
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-int inc;
-unsigned long n1 = 0, n2 = 1, n3;
-for (inc = 0; inc < 50; inc++)
-{
-n3 = n1 + n2;
-printf("%lu", n3);
-n1 = n2;
-n2 = n3;
 
-if (inc == 49)
-printf("\n");
-else
-printf(", ");
-
-}
-
-return (0);
+	return (0);
 }
