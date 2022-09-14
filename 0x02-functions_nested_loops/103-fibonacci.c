70b633a0cff6655391dffa082fc050dd65d78181
baigag <106968031+baigag@users.noreply.github.com>
2022-09-14 13:34:19 -0700

    task 15

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -1,13 +1,23 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include "holberton.h"
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
+*main - prints sum of even fibonacci
+*suit elements
+*fibonacci suit numbers
+*Return: return 0
+*/
 int main(void)
 {
+unsigned long n1 = 0, n2 = 1, n3 = 0, sum = 0;
+while (n3 <= 4000000)
+{
+n3 = n1 + n2;
+n1 = n2;
+n2 = n3;
 
-	return (0);
+if ((n1 % 2) == 0)
+sum += n1;
+
+}
+printf("%ld\n", sum);
+return (0);
 }
